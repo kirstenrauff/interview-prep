bb3c7d4c2b55ddb7bbd2b683968bba6aa94503f4
kirstenandizzy <kirsten.rauffer+1@gmail.com>
2026-06-28 09:51:05 -0500

    Addding env url

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 4201,
-    strictPort: true, 
-    proxy: {
-      '/api': 'http://localhost:4200/'
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const apiProxyTarget = env.API_HOST || 'http://localhost:4200'
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 4201,
+      strictPort: true,
+      proxy: {
+        '/api': apiProxyTarget
+      }
     }
-  },
+  }
 })
